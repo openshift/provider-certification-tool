2e2f7ca2b1ed426bd284166676b5ba7f1b8e71b4
Marco Braga <mrbraga@redhat.com>
2022-05-18 19:14:39 -0300

    feat(plugin-cert): improve plugin builder to fixed sonobuoy version

--- tools/openshift-provider-cert-plugin/hack/build-image.sh
+++ tools/openshift-provider-cert-plugin/hack/build-image.sh
@@ -6,12 +6,26 @@ set -o errexit
 
 registry="quay.io/mrbraga"
 
+
 # build openshift-tests image (@openshift/origin)
 test "$(podman image exists openshift-tests:latest; echo $?)" -eq 0 || \
     "$(dirname "$0")"/build-openshift-tests-image.sh
 
 # generate tests
 "$(dirname "$0")"/generate-tests-tiers.sh
+"$(dirname "$0")"/generate-tests-exception.sh
+
+# Download Sonobuoy
+sb_version="0.56.6"
+sb_filename="sonobuoy_${sb_version}_linux_amd64.tar.gz"
+sb_url="https://github.com/vmware-tanzu/sonobuoy/releases/download/v${sb_version}/${sb_filename}"
+
+mkdir -p ./tmp
+rm -rvf ./tmp/* ./sonobuoy
+
+wget ${sb_url} -P ./tmp
+tar xfz ./tmp/${sb_filename} sonobuoy
+./sonobuoy version
 
 # create plugin image
 podman build -t ${registry}/openshift-tests-provider-cert:latest .
